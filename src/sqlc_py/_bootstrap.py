23c89d3bdece6361df7994c47d52e53cf972a7db
Rayakame <github.android206@passmail.net>
2025-04-30 23:28:20 +0200

    build: add sqlc script to pyproject.toml (master)

---
+++ src/sqlc_py/_bootstrap.py
@@ -0,0 +1,14 @@
+from importlib import resources
+import os, stat, subprocess, sys
+
+def _bin():
+    name = "sqlc.exe" if os.name == "nt" else "sqlc"
+    path = resources.files(__package__) / "_vendor" / name
+    if os.name != "nt":
+        mode = os.stat(path).st_mode
+        if not (mode & stat.S_IXUSR):
+            os.chmod(path, mode | stat.S_IXUSR)
+    return str(path)
+
+def main():
+    sys.exit(subprocess.call([_bin(), *sys.argv[1:]]))
